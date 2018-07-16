2ab571456d727b614c2f7992fc96c791d4980104
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-16 22:45:59 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,9 @@
 def using_until
        levitation_force = 0
        until levitation_force = 10
-        
+      puts "Wingardium Leviosa"
         levitation_force += 1
       end 
-      puts "Wingardium Leviosa"
+
 end
 
